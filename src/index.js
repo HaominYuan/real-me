f66f7e533a9fe4014d55267693c9a982667a4ed2
tstxxy <tstxxy@gmail.com>
2020-10-13 00:20:03 +0800

    change to 井字棋

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 // eslint-disable-next-line
-import React, {Component} from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import CommentApp from "./comment/CommentApp";
 // eslint-disable-next-line
 import Game from "./game/Game"
-import "./index.scss" 
+import "./index.scss"
 
-// ReactDOM.render(<Game />, document.getElementById("root"));
+// eslint-disable-next-line
+class Index extends Component {
+    render() {
+        return (
+            <div className='index'>
+                <Game />
+                <CommentApp />
+            </div>
+        )
+    }
+}
 
-ReactDOM.render(<CommentApp />, document.getElementById("root"));
+ReactDOM.render(<Game />, document.getElementById("root"));
+// ReactDOM.render(<CommentApp />, document.getElementById("root"));
+
+// ReactDOM.render(<Index />, document.getElementById("root"))
