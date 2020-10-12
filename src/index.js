a845310b53648b46378e3c01ff550fc59d660816
tstxxy <tstxxy@gmail.com>
2020-10-12 22:54:09 +0800

    fix bug: rename game.js => Game.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import CommentApp from "./comment/CommentApp";
 import Game from "./game/Game"
 import "./index.scss" 
-
-
 // ReactDOM.render(<Game />, document.getElementById("root"));
 
 ReactDOM.render(<CommentApp />, document.getElementById("root"));
